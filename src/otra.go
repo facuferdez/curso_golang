f86fe20caf0947ce3c68f00db4c496b2fb21db6f
facuferdez <facundonfernandez98@gmail.com>
2022-04-16 19:20:53 +0000

    Practicamos un ratico

--- src/otra.go
+++ src/otra.go
@@ -2,7 +2,31 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("locaaaaaaaaaaaaaaa
-	")
+func main() {
+	var slice []int
+	var valor int
+	var suma = 0
+
+	for {
+		fmt.Println("Ingrese los valores (-1 para terminar):")
+		fmt.Scan(&valor)
+		if valor == -1 {
+			break
+		}
+		slice = append(slice, valor)
+		suma = suma + valor
+	}
+
+	promedio := suma / len(slice)
+	mayProm := 0
+	for i := 0; i < len(slice); i++ {
+		if slice[i] > promedio {
+			mayProm++
+		}
+	}
+
+	fmt.Println("EL slice completo es:")
+	fmt.Println(slice)
+	fmt.Println("Promedio:", promedio)
+	fmt.Println("La cantidad de productos ingresados mayores al promedio es:", mayProm)
 }
